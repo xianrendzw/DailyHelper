61068f92f31e90b80f59453a21d2ee63b9bdd065
xianrendzw <14068728@qq.com>
2017-01-06 15:45:09 +0800

    init projects

---
+++ dailyhelper-jersey/src/main/java/com/easytoolsoft/dailyhelper/jersey/DailyHelperApplication.java
@@ -0,0 +1,12 @@
+package com.easytoolsoft.dailyhelper.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyHelperApplication.class, args);
+	}
+}
